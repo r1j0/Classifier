3537326606199c6bfc58ac141574685c31106f49
r1j0 <johndoe@local.dev>
2011-06-25 22:40:10 +0200

    added javadoc

--- app/libs/classifier/classifier_store.php
+++ app/libs/classifier/classifier_store.php
@@ -3,9 +3,24 @@
 interface ClassifierStore {
 
 
+	/**
+	 * Accepts an array of tokens and returns an array of ClassifierObjects.
+	 *
+	 * It is important to return all tokens. Otherwise unknown tokens can
+	 * not contribute in a meaningfull way.
+	 *
+	 * @param array $tokens
+	 * @return ClassifierObjects
+	 */
 	public function get($tokens);
 
 
+	/**
+	 * The array of ClassifierObjects should only contain a unique list of
+	 * ClassifierObjects.
+	 *
+	 * @param array ClassifierObjects $classifierObjects
+	 */
 	public function update($classifierObjects);
 
 }
